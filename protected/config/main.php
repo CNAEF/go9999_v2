e3ad1ee5fd3dacc4809c25b026d4a136becd69e5
m4ker <makerwang@gmail.com>
2015-11-15 15:24:18 +0800

    hide index.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -49,6 +49,8 @@ return array(
         // uncomment the following to enable URLs in path-format
         'urlManager' => array(
             'urlFormat' => 'path',
+            'showScriptName'=>false,
+            'caseSensitive'=>false,
             'rules' => array(
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
